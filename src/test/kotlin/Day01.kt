f7cd58786e1153368a18b39283d22836cde9ed9f
Christof Vollrath <christof@taobits.net>
2023-12-01 23:29:26 +0100

    Day01 Part1

--- src/test/kotlin/Day01.kt
+++ src/test/kotlin/Day01.kt
@@ -1,17 +1,45 @@
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.core.spec.style.FunSpec
-import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 val exampleInput = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
     """.trimIndent()
 
 class Day01Part1: BehaviorSpec() { init {
     Given("example input") {
-        When("something") {
+        val calibrations = parseCalibrations(exampleInput)
+        Then("should have found 4 calibrations") {
+            calibrations.size shouldBe 4
         }
-        Then("anything") {
-            5 shouldBe 5
+        Then("first calibration should be parsed correctly") {
+            calibrations[0] shouldBe 12
+        }
+        Then("and third") {
+            calibrations[2] shouldBe 15
+        }
+        When("calculating the sum of calibrations") {
+            val sum = calibrations.sum()
+            Then("sum should be right") {
+                sum shouldBe 142
+            }
+        }
+    }
+    Given("exercise input") {
+        val calibrations = parseCalibrations(readResource("inputDay01.txt")!!)
+        When("calculating the sum of calibrations") {
+            val sum = calibrations.sum()
+            Then("it should find the solution for part 1") {
+                sum shouldBe 55_123
+            }
         }
     }
 } }
+
+fun parseCalibrations(input: String) = input.split("\n").map { line ->
+    val firstDigit = line.first { it.isDigit() }
+    val lastDigit = line.last { it.isDigit() }
+    "$firstDigit$lastDigit".toInt()
+}
