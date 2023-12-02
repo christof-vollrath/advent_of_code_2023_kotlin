152a442e213d64828622a1332633e6a2b8fca418
Christof Vollrath <christof@taobits.net>
2023-12-02 10:41:49 +0100

    Day02 part 2

--- src/test/kotlin/Day02.kt
+++ src/test/kotlin/Day02.kt
@@ -3,14 +3,15 @@ import io.kotest.matchers.comparables.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
 
 
-class Day02Part1: BehaviorSpec() { init {
-    val exampleInput = """
+val exampleInput = """
         Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
         Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
         Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
         Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
         Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     """.trimIndent()
+
+class Day02Part1: BehaviorSpec() { init {
 
     val restriction = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
@@ -36,15 +37,50 @@ class Day02Part1: BehaviorSpec() { init {
 
     Given("exercise input") {
         val cubeGames = parseCubeGames(readResource("inputDay02.txt")!!)
-        When("calculating the sum of games fullfilling the restriction") {
+        When("finding the sum of games fulfilling the restriction") {
             val allowedGames = cubeGames.filter { checkCubeGameRestriction(it, restriction) }.map { it.id }
-            val result = allowedGames.sum()
-            result shouldBeGreaterThan 2183
-            result shouldBe 2447
+            Then("it should calculate the right sum") {
+                val result = allowedGames.sum()
+                result shouldBeGreaterThan 2183
+                result shouldBe 2447
+            }
         }
     }
 
 } }
+
+class Day02Part2: BehaviorSpec() { init {
+
+    Given("example input") {
+        val cubeGames = parseCubeGames(exampleInput)
+        Then("it should find the fewest number of cubes for game 1") {
+            val game1 = cubeGames[0]
+            fewestNumberCubes(game1) shouldBe mapOf( "red" to 4, "green" to 2, "blue" to 6 )
+        }
+        Then("it should find the power (as defined in exercise) of all cubes") {
+            val gamePowers = cubeGames.map { calculateGamePower(it) }
+            gamePowers shouldBe listOf(48, 12, 1560, 630, 36)
+            gamePowers.sum() shouldBe 2286
+        }
+    }
+    Given("exercise input") {
+        val cubeGames = parseCubeGames(readResource("inputDay02.txt")!!)
+        When("calculating the power of all games") {
+            val gamePowers = cubeGames.map { calculateGamePower(it) }
+            Then("it should calculate the right sum") {
+                gamePowers.sum() shouldBe 56322
+            }
+        }
+    }
+
+} }
+
+private fun fewestNumberCubes(game: CubeGame) = listOf("red", "green", "blue").associateWith { color ->
+    val nrs = game.moves.map { colorsWithNr -> colorsWithNr.getOrDefault(color, 0) }
+    nrs.max()
+}
+
+private fun calculateGamePower(game: CubeGame) = fewestNumberCubes(game).values.fold(1) { r, t -> r * t }
 
 private fun checkCubeGameRestriction(cubeGame: CubeGame, restriction: Map<String, Int>) = cubeGame.moves.all { cubesWithNr ->
     restriction.entries.all { (color, nr ) ->
