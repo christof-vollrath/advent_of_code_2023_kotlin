96364538096fd5a9654e376555e18df6fd161f3e
Christof Vollrath <christof@taobits.net>
2023-12-13 22:01:15 +0100

    Day13 part 2

--- src/test/kotlin/Day13.kt
+++ src/test/kotlin/Day13.kt
@@ -73,6 +73,56 @@ class Day13Part1: BehaviorSpec() { init {
 
 } }
 
+class Day13Part2: BehaviorSpec() { init {
+
+    Given("example input") {
+        When("parsing the input") {
+            val mirrorMaps = parseMirrorMaps(exampleInputDay13)
+            Then("finding horizontal mirror for first map should not find one") {
+                findHorizontalMirrorWithSmug(mirrorMaps[0]) shouldBe 3
+            }
+            Then("finding horizontal mirror with one smug for 2nd map should work") {
+                findHorizontalMirrorWithSmug(mirrorMaps[1]) shouldBe 1
+            }
+            Then("finding vertical mirror with one smug for first map should should work") {
+                findVerticalMirrorWithSmug(mirrorMaps[0]) shouldBe 0
+            }
+            Then("finding vertical mirror with one smug for 2nd map not find one") {
+                findVerticalMirrorWithSmug(mirrorMaps[1]) shouldBe 0
+            }
+            When("finding mirrors and summing them") {
+                val result = mirrorMaps.sumOf { findHorizontalMirrorWithSmug(it) * 100 + findVerticalMirrorWithSmug(it) }
+                Then("it should find the solution") {
+                    result shouldBe 400
+                }
+            }
+        }
+    }
+
+    Given("a 2d list") {
+        Then("pivot should turn is to the side") {
+            pivot(listOf(
+                listOf('#', '#'),
+                listOf('.', '.')
+            )) shouldBe listOf(
+                listOf('#', '.'),
+                listOf('#', '.')
+            )
+        }
+    }
+
+    Given("exercise input") {
+        val mirrorMaps = parseMirrorMaps(readResource("inputDay13.txt")!!)
+        When("finding mirrors and summing them") {
+            val result = mirrorMaps.sumOf { findHorizontalMirrorWithSmug(it) * 100 + findVerticalMirrorWithSmug(it) }
+            Then("it should find the solution") {
+                result shouldBe 31539
+            }
+        }
+    }
+
+} }
+
 fun parseMirrorMaps(input: String) = buildList {
     var parsedMap = mutableListOf<List<Char>>()
     for (line in input.split("\n").map { it.trim() }) {
@@ -100,6 +150,30 @@ fun findHorizontalMirror(map: List<List<Char>>): Int {
 }
 
 fun findVerticalMirror(map: List<List<Char>>) = findHorizontalMirror(pivot(map))
+
+fun findHorizontalMirrorWithSmug(map: List<List<Char>>): Int {
+    fun countSmugs(l1: List<Char>, l2: List<Char>): Int {
+        var count = 0
+        for(i in l1.indices)
+            if (l1[i] != l2[i]) count++
+        return count
+    }
+    fun isMirror(pos: Int): Boolean {
+        var nrSmugs = 0
+        var i1 = pos - 1
+        var i2 = pos
+        while(i1 >= 0 && i2 < map.size) {
+            nrSmugs += countSmugs(map[i1], map[i2])
+            i1--; i2++
+        }
+        return nrSmugs == 1
+    }
+    for(i in 1 until map.size)
+        if (isMirror(i)) return i
+    return 0
+}
+
+fun findVerticalMirrorWithSmug(map: List<List<Char>>) = findHorizontalMirrorWithSmug(pivot(map))
 
 fun pivot(list2d: List<List<Char>>) = buildList {
     var resultLine = mutableListOf<Char>()
