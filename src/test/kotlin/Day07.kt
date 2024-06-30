cf5f9d22fe544662ecd811b6823911eaeece02b5
Christof Vollrath <christof@taobits.net>
2024-06-30 21:30:38 +0200

    Day 7 part 2 solved

--- src/test/kotlin/Day07.kt
+++ src/test/kotlin/Day07.kt
@@ -1,4 +1,5 @@
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.ints.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
 import java.util.Comparator.comparing
 
@@ -10,7 +11,7 @@ val exampleInputDay07 = """
     QQQJA 483
     """.trimIndent()
 
-class Day07Part: BehaviorSpec() { init {
+class Day07Part1: BehaviorSpec() { init {
     Given("example input") {
         When("parsing the input") {
             val cardAndBids = parseCardAndBids(exampleInputDay07)
@@ -94,8 +95,45 @@ class Day07Part: BehaviorSpec() { init {
             calculateTotalWinnings(cardAndBids) shouldBe 252_295_678
         }
     }
-}
-}
+} }
+
+class Day07Part2: BehaviorSpec() { init {
+    Given("some card hands") {
+        When("calculating type rank") {
+            typeRank2("32T3K") shouldBe 2
+            typeRank2("T55J5") shouldBe 6
+            typeRank2("KK677") shouldBe 3
+            typeRank2("KTJJT") shouldBe 6
+            typeRank2("QQQJA") shouldBe 6
+            typeRank2("TJTTJ") shouldBe 7
+            typeRank2("999JJ") shouldBe 7
+        }
+        When("compareHands2") {
+            compareHands2("TJTTJ", "TJTTJ") shouldBe 0
+            compareHands2("TJTTJ", "TJTTK") shouldBe 1
+            compareHands2("TJTTJ", "TJTTT") shouldBe -1
+            compareHands2("TJTTJ", "999JJ") shouldBe 1
+        }
+    }
+    Given("example input") {
+        When("parsing the input") {
+            val cardAndBids = parseCardAndBids(exampleInputDay07)
+
+            When("calculating total winnings") {
+                calculateTotalWinnings2(cardAndBids) shouldBe 5905
+            }
+        }
+    }
+    Given("exercise input") {
+        val cardAndBids = parseCardAndBids(readResource("inputDay07.txt")!!)
+        cardAndBids.size shouldBe 1000
+        When("calculating total winnings") {
+            val totalWinnings = calculateTotalWinnings2(cardAndBids)
+            totalWinnings shouldBeGreaterThan  250515608
+            totalWinnings shouldBe  250_577_259
+        }
+    }
+} }
 
 fun Char.strength() = if (isDigit()) digitToInt()
 else {
@@ -108,7 +146,6 @@ else {
         else -> throw IllegalArgumentException("Unexpected card $this")
     }
 }
-
 
 fun parseCardAndBids(input: String) = input.split("\n").map {
     val (cards, bidString) = it.split(" ")
@@ -153,6 +190,22 @@ fun typeRank(cards: String): Int {
         }
 }
 
+fun typeRank2(cards: String): Int {
+    val jokerSize = cards.filter { it == 'J' }.length
+    if (jokerSize == 5) return 7
+    val cardsWithoutJokers = cards.filter { it != 'J' }
+    val groupedCardsSizes = cardsWithoutJokers.groupBy { it }.values.sortedByDescending { it.size }.map { it.size }
+    return when {
+        groupedCardsSizes[0] + jokerSize == 5 -> 7
+        groupedCardsSizes[0] + jokerSize == 4 -> 6
+        groupedCardsSizes[0] + jokerSize == 3 && groupedCardsSizes[1] == 2 -> 5 // no need to add joker to second part, because adding to first is always better
+        groupedCardsSizes[0] + jokerSize == 3 -> 4
+        groupedCardsSizes[0] == 2 && groupedCardsSizes[1] == 2 -> 3 // no need to add to one part since it's always considered before
+        groupedCardsSizes[0] + jokerSize == 2 -> 2
+        else -> 1
+    }
+}
+
 fun compareHands(hand1: String, hand2: String): Int {
     val typeRank1 = typeRank(hand1)
     val typeRank2 = typeRank(hand2)
@@ -169,3 +222,48 @@ fun sortAndAddRank(cardAndBids: List<Pair<String, Int>>) =
 
 fun calculateTotalWinnings(cardAndBids: List<Pair<String, Int>>) =
     sortAndAddRank(cardAndBids).sumOf { (_, bid, rank) -> bid * rank }
+
+/*
+fun calculateTotalWinnings2(cardAndBids: List<Pair<String, Int>>) =
+    sortAndAddRank2(cardAndBids).sumOf { (_, bid, rank) -> bid * rank }
+ */
+fun calculateTotalWinnings2(cardAndBids: List<Pair<String, Int>>): Int {
+    val sorted = sortAndAddRank2(cardAndBids)
+    return sorted.sumOf { (_, bid, rank) -> bid * rank }
+}
+
+fun sortAndAddRank2(cardAndBids: List<Pair<String, Int>>) =
+    cardAndBids.sortedWith(comparing(
+        { it.first },
+        { h1, h2 -> compareHands2(h1, h2) }
+    )).mapIndexed{ i, (c, b) -> Triple(c, b, i+1)}
+
+fun Char.strength2() = if (isDigit()) digitToInt() + 1
+else {
+    when(this) {
+        'A' -> 14
+        'K' -> 13
+        'Q' -> 12
+        'T' -> 11
+        'J' -> 1
+        else -> throw IllegalArgumentException("Unexpected card $this")
+    }
+}
+
+fun compareHandRanks2(hand1: String, hand2: String) = if (hand1 == hand2) 0
+else {
+    val c1c2 = hand1.zip(hand2).firstOrNull { (c1, c2) -> c1.strength2() != c2.strength2() }
+    if (c1c2 == null) 0
+    else {
+        val (c1, c2) = c1c2
+        c1.strength2().compareTo(c2.strength2())
+    }
+}
+
+fun compareHands2(hand1: String, hand2: String): Int {
+    val typeRank1 = typeRank2(hand1)
+    val typeRank2 = typeRank2(hand2)
+    val comp = typeRank1.compareTo(typeRank2)
+    return if (comp != 0) comp
+    else compareHandRanks2(hand1, hand2)
+}
